74acccf8721d392a2ceac3e3c9492fb5a3add944
Estevan Hernades Silva de Oliveira <estevanhernandes1@gmail.com>
2026-05-18 20:38:14 -0300

    feat: configuração banco de dados e criação de entidades

---
+++ src/main/java/com/example/springsecurity/entities/Role.java
@@ -0,0 +1,22 @@
+package com.example.springsecurity.entities;
+
+import com.example.springsecurity.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+}
---
+++ src/main/java/com/example/springsecurity/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.example.springsecurity.enums;
+
+public enum RoleName {
+    ROLE_CUSTOMER,
+    ROLE_ADMINISTRATOR
+}
